516087a017261f2c567e17667533564285a0e973
perry <ck0300847@gmail.com>
2015-03-22 17:32:43 +0800

    first checkin
    add files

---
+++ www/coc tool/script/data_defense.js
@@ -0,0 +1,824 @@
+
+var defense_cannon  = new Array();
+defense_cannon[1] = 
+{
+	gold:250, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:1, 
+	build_time:1
+};
+defense_cannon[2] = 
+{
+	gold:1000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:1, 
+	build_time:15
+};
+defense_cannon[3] = 
+{
+	gold:4000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:2, 
+	build_time:45
+};
+defense_cannon[4] = 
+{
+	gold:16000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:3, 
+	build_time:2*60
+};
+defense_cannon[5] = 
+{
+	gold:50000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:4, 
+	build_time:6*60
+};
+defense_cannon[6] = 
+{
+	gold:100000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:12*60
+};
+defense_cannon[7] = 
+{
+	gold:200000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:6, 
+	build_time:1*60*24
+};
+defense_cannon[8] = 
+{
+	gold:400000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:2*60*24
+};
+defense_cannon[9] = 
+{
+	gold:800000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:3*60*24
+};
+defense_cannon[10] = 
+{
+	gold:1600000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:4*60*24
+};
+defense_cannon[11] = 
+{
+	gold:3200000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:5*60*24
+};
+defense_cannon[12] = 
+{
+	gold:6400000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:6*60*24
+};
+defense_cannon[13] = 
+{
+	gold:7500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:7*60*24
+};
+
+var defense_archer_tower  = new Array();
+defense_archer_tower[1] = 
+{
+	gold:1000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:2, 
+	build_time:15
+};
+defense_archer_tower[2] = 
+{
+	gold:2000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:2, 
+	build_time:30
+};
+defense_archer_tower[3] = 
+{
+	gold:5000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:3, 
+	build_time:45
+};
+defense_archer_tower[4] = 
+{
+	gold:20000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:4, 
+	build_time:4*60
+};
+defense_archer_tower[5] = 
+{
+	gold:80000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:12*60
+};
+defense_archer_tower[6] = 
+{
+	gold:180000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:1*60*24
+};
+defense_archer_tower[7] = 
+{
+	gold:360000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:6, 
+	build_time:2*60*24
+};
+defense_archer_tower[8] = 
+{
+	gold:720000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:3*60*24
+};
+defense_archer_tower[9] = 
+{
+	gold:1500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:4*60*24
+};
+defense_archer_tower[10] = 
+{
+	gold:2500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:5*60*24
+};
+defense_archer_tower[11] = 
+{
+	gold:4500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:6*60*24
+};
+defense_archer_tower[12] = 
+{
+	gold:6500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:7*60*24
+};
+defense_archer_tower[13] = 
+{
+	gold:7500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:7*60*24
+};
+
+var defense_mortar  = new Array();
+defense_mortar[1] = 
+{
+	gold:8000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:3, 
+	build_time:8*60
+};
+defense_mortar[2] = 
+{
+	gold:32000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:4, 
+	build_time:12*60
+};
+defense_mortar[3] = 
+{
+	gold:120000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:1*60*24
+};
+defense_mortar[4] = 
+{
+	gold:400000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:6, 
+	build_time:2*60*24
+};
+defense_mortar[5] = 
+{
+	gold:800000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:4*60*24
+};
+defense_mortar[6] = 
+{
+	gold:1600000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:5*60*24
+};
+defense_mortar[7] = 
+{
+	gold:3200000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:7*60*24
+};
+defense_mortar[8] = 
+{
+	gold:6400000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:10*60*24
+};
+
+
+var defense_air_defense  = new Array();
+defense_air_defense[1] = 
+{
+	gold:22500, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:4, 
+	build_time:5*60
+};
+defense_air_defense[2] = 
+{
+	gold:90000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:4, 
+	build_time:1*60*24
+};
+defense_air_defense[3] = 
+{
+	gold:270000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:3*60*24
+};
+defense_air_defense[4] = 
+{
+	gold:540000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:6, 
+	build_time:5*60*24
+};
+defense_air_defense[5] = 
+{
+	gold:1080000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:6*60*24
+};
+defense_air_defense[6] = 
+{
+	gold:2160000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:8*60*24
+};
+defense_air_defense[7] = 
+{
+	gold:3200000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:10*60*24
+};
+defense_air_defense[8] = 
+{
+	gold:7560000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:12*60*24
+};
+
+
+
+var defense_wizard_tower  = new Array();
+defense_wizard_tower[1] = 
+{
+	gold:180000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:12*60
+};
+defense_wizard_tower[2] = 
+{
+	gold:360000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:1*60*24
+};
+defense_wizard_tower[3] = 
+{
+	gold:720000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:6, 
+	build_time:2*60*24
+};
+defense_wizard_tower[4] = 
+{
+	gold:1280000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:3*60*24
+};
+defense_wizard_tower[5] = 
+{
+	gold:1960000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:4*60*24
+};
+defense_wizard_tower[6] = 
+{
+	gold:2680000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:5*60*24
+};
+defense_wizard_tower[7] = 
+{
+	gold:5360000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:7*60*24
+};
+defense_wizard_tower[8] = 
+{
+	gold:6480000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:10*60*24
+};
+
+
+
+
+var defense_hidden_tesla  = new Array();
+defense_wizard_tower[1] = 
+{
+	gold:1000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:1*60*24
+};
+defense_wizard_tower[2] = 
+{
+	gold:1250000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:3*60*24
+};
+defense_wizard_tower[3] = 
+{
+	gold:1500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:5*60*24
+};
+defense_wizard_tower[4] = 
+{
+	gold:2000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:6*60*24
+};
+defense_wizard_tower[5] = 
+{
+	gold:2500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:8*60*24
+};
+defense_wizard_tower[6] = 
+{
+	gold:3000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:10*60*24
+};
+defense_wizard_tower[7] = 
+{
+	gold:3500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:12*60*24
+};
+defense_wizard_tower[8] = 
+{
+	gold:5000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:14*60*24
+};
+
+
+var defense_x_bow  = new Array();
+defense_x_bow[1] = 
+{
+	gold:3000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:7*60*24
+};
+defense_x_bow[2] = 
+{
+	gold:5000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:10*60*24
+};
+defense_x_bow[3] = 
+{
+	gold:7000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:14*60*24
+};
+defense_x_bow[4] = 
+{
+	gold:8000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:14*60*24
+};
+
+
+var defense_inferno_tower  = new Array();
+defense_inferno_tower[1] = 
+{
+	gold:5000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:7*60*24
+};
+defense_inferno_tower[2] = 
+{
+	gold:6500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:10*60*24
+};
+defense_inferno_tower[3] = 
+{
+	gold:8000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:14*60*24
+};
+
+
+var defense_wall  = new Array();
+defense_wall[1] = 
+{
+	gold:200, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:2, 
+	build_time:0
+};
+defense_wall[2] = 
+{
+	gold:1000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:2, 
+	build_time:0
+};
+defense_wall[3] = 
+{
+	gold:5000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:3, 
+	build_time:0
+};
+defense_wall[4] = 
+{
+	gold:10000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:4, 
+	build_time:0
+};
+defense_wall[5] = 
+{
+	gold:30000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:0
+};
+defense_wall[6] = 
+{
+	gold:75000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:6, 
+	build_time:0
+};
+defense_wall[7] = 
+{
+	gold:200000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:0
+};
+defense_wall[8] = 
+{
+	gold:500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:0
+};
+defense_wall[9] = 
+{
+	gold:1000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:0
+};
+defense_wall[10] = 
+{
+	gold:3000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:0
+};
+defense_wall[11] = 
+{
+	gold:4000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:0
+};
+
+
+var trap_bomb  = new Array();
+trap_bomb[1] = 
+{
+	gold:400, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:3, 
+	build_time:0
+};
+trap_bomb[2] = 
+{
+	gold:1000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:3, 
+	build_time:0
+};
+trap_bomb[3] = 
+{
+	gold:10000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:0
+};
+trap_bomb[4] = 
+{
+	gold:100000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:0
+};
+trap_bomb[5] = 
+{
+	gold:1000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:0
+};
+trap_bomb[6] = 
+{
+	gold:1500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:0
+};
+
+var trap_spring_trap  = new Array();
+trap_spring_trap[1] = 
+{
+	gold:2000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:1, 
+	build_time:0
+};
+
+
+
+var trap_giant_bomb  = new Array();
+trap_giant_bomb[1] = 
+{
+	gold:12500, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:6, 
+	build_time:0
+};
+trap_giant_bomb[2] = 
+{
+	gold:75000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:6, 
+	build_time:0
+};
+trap_giant_bomb[3] = 
+{
+	gold:750000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:0
+};
+trap_giant_bomb[4] = 
+{
+	gold:2500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:0
+};
+
+
+
+var trap_giant_bomb  = new Array();
+trap_giant_bomb[1] = 
+{
+	gold:4000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:0
+};
+trap_giant_bomb[2] = 
+{
+	gold:20000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:5, 
+	build_time:0
+};
+trap_giant_bomb[3] = 
+{
+	gold:200000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:0
+};
+trap_giant_bomb[4] = 
+{
+	gold:1500000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:0
+};
+
+
+
+var trap_seeking_air_mine  = new Array();
+trap_seeking_air_mine[1] = 
+{
+	gold:15000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:7, 
+	build_time:0
+};
+trap_seeking_air_mine[2] = 
+{
+	gold:2000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:0
+};
+trap_seeking_air_mine[3] = 
+{
+	gold:4000000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:10, 
+	build_time:0
+};
+
+
+
+var trap_skeleton_trap  = new Array();
+trap_skeleton_trap[1] = 
+{
+	gold:6000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:0
+};
+trap_skeleton_trap[2] = 
+{
+	gold:600000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:8, 
+	build_time:0
+};
+trap_skeleton_trap[3] = 
+{
+	gold:1300000, 
+	elixir:0, 
+	dark_elxir:0, 
+	town_level:9, 
+	build_time:0
+};
